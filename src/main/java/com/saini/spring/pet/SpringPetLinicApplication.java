4dbc4c34a018034ea304f794910f80d056831717
vinaysaini422 <vinaysaini422@gmail.com>
2020-01-05 13:30:48 +0530

    initial commit

---
+++ src/main/java/com/saini/spring/pet/SpringPetLinicApplication.java
@@ -0,0 +1,13 @@
+package com.saini.spring.pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetLinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPetLinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saini/spring/pet/SpringPetLinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saini.spring.pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetLinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
